7e1089189b8bc35ddab199075f31653c56f4b0b9
Adraas <pickalov.artyom@yandex.ru>
2018-04-07 10:13:00 +0400

    Basic functional

--- server/src/main/java/ru/wkn/timekeepingsystem/model/branchoffice/BranchOffice.java
+++ server/src/main/java/ru/wkn/timekeepingsystem/model/branchoffice/BranchOffice.java
@@ -1,4 +1,10 @@
 package ru.wkn.timekeepingsystem.model.branchoffice;
 
 public class BranchOffice {
+
+    private String branchOfficeName;
+
+    public BranchOffice(String branchOfficeName) {
+        this.branchOfficeName = branchOfficeName;
+    }
 }

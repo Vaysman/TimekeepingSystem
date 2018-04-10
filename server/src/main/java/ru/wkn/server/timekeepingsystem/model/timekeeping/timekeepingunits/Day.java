f0cfc3581e20406b6ac703184bdaa8646701b5ae
Adraas <pickalov.artyom@yandex.ru>
2018-04-10 08:52:31 +0400

    The list of fields is changed

--- server/src/main/java/ru/wkn/server/timekeepingsystem/model/timekeeping/timekeepingunits/Day.java
+++ server/src/main/java/ru/wkn/server/timekeepingsystem/model/timekeeping/timekeepingunits/Day.java
@@ -1,10 +1,15 @@
 package ru.wkn.server.timekeepingsystem.model.timekeeping.timekeepingunits;
 
-import ru.wkn.server.timekeepingsystem.model.dao.JdbcEventDao;
-import ru.wkn.server.timekeepingsystem.model.dao.JdbcTaskDao;
+import ru.wkn.server.timekeepingsystem.model.branchoffice.department.employee.Employee;
+import ru.wkn.server.timekeepingsystem.model.timekeeping.timekeepingunits.event.TimekeepingEvent;
+import ru.wkn.server.timekeepingsystem.model.timekeeping.timekeepingunits.task.Task;
+
+import java.util.List;
 
 public class Day {
 
-    private JdbcTaskDao jdbcTaskDao;
-    private JdbcEventDao jdbcEventDao;
+    private String date;
+    private List<Employee> employees;
+    private List<Task> tasks;
+    private List<TimekeepingEvent> events;
 }

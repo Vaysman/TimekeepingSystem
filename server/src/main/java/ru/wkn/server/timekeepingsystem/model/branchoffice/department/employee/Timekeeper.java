916b6a2bce269a7a90d88bb8f687845bcecc795c
Adraas <pickalov.artyom@yandex.ru>
2018-04-10 08:42:56 +0400

    The list of fields is changed

--- server/src/main/java/ru/wkn/server/timekeepingsystem/model/branchoffice/department/employee/Timekeeper.java
+++ server/src/main/java/ru/wkn/server/timekeepingsystem/model/branchoffice/department/employee/Timekeeper.java
@@ -1,11 +1,11 @@
 package ru.wkn.server.timekeepingsystem.model.branchoffice.department.employee;
 
-import ru.wkn.server.timekeepingsystem.model.dao.Dao;
+import ru.wkn.server.timekeepingsystem.model.timekeeping.managers.DayManager;
 import ru.wkn.server.timekeepingsystem.model.timekeeping.summary.TimekeepingLog;
 
 public class Timekeeper {
 
     private Employee employee;
-    private Dao<Employee> dao;
+    private DayManager dayManager;
     private TimekeepingLog timekeepingLog;
 }

0712cdbbdf820f333260ab03be713741728d9a64
Adraas <pickalov.artyom@yandex.ru>
2018-04-10 08:48:24 +0400

    The list of fields is changed

--- server/src/main/java/ru/wkn/server/timekeepingsystem/model/timekeeping/managers/TaskManager.java
+++ server/src/main/java/ru/wkn/server/timekeepingsystem/model/timekeeping/managers/TaskManager.java
@@ -1,8 +1,11 @@
 package ru.wkn.server.timekeepingsystem.model.timekeeping.managers;
 
+import ru.wkn.server.timekeepingsystem.model.dao.Dao;
 import ru.wkn.server.timekeepingsystem.model.timekeeping.timekeepingunits.task.Task;
 
 public class TaskManager {
+
+    private Dao<Task> taskDao;
 
     public Task createTask(String definition, String startTime, String endTime) {
         return new Task(definition, startTime, endTime);

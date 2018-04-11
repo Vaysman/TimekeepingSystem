aa00202f708bfe85fcd694c7185299475b49d0ff
Adraas <pickalov.artyom@yandex.ru>
2018-04-11 10:14:10 +0400

    List of fields is chaged

--- server/src/main/java/ru/wkn/server/timekeepingsystem/model/timekeeping/timekeepingunits/task/Task.java
+++ server/src/main/java/ru/wkn/server/timekeepingsystem/model/timekeeping/timekeepingunits/task/Task.java
@@ -7,24 +7,42 @@ public class Task {
     private String definition;
     private String startTime;
     private String endTime;
+    private int employeeID;
     private boolean isAccomplished;
 
-    public Task(String definition, String startTime, String endTime) {
+    public Task(String definition, String startTime, String endTime, int employeeID) {
         this.definition = definition;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.employeeID = employeeID;
     }
 
     public String getDefinition() {
         return definition;
     }
 
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
     public String getStartTime() {
         return startTime;
     }
 
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
     public String getEndTime() {
         return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getEmployeeID() {
+        return employeeID;
     }
 
     public boolean isAccomplished() {
@@ -40,7 +58,8 @@ public class Task {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Task task = (Task) o;
-        return isAccomplished == task.isAccomplished &&
+        return employeeID == task.employeeID &&
+                isAccomplished == task.isAccomplished &&
                 Objects.equals(definition, task.definition) &&
                 Objects.equals(startTime, task.startTime) &&
                 Objects.equals(endTime, task.endTime);
@@ -48,7 +67,7 @@ public class Task {
 
     @Override
     public int hashCode() {
-        return Objects.hash(definition, startTime, endTime, isAccomplished);
+        return Objects.hash(definition, startTime, endTime, employeeID, isAccomplished);
     }
 
     @Override
@@ -57,6 +76,7 @@ public class Task {
                 "definition='" + definition + '\'' +
                 ", startTime='" + startTime + '\'' +
                 ", endTime='" + endTime + '\'' +
+                ", employeeID=" + employeeID +
                 ", isAccomplished=" + isAccomplished +
                 '}';
     }
